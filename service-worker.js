970b99ed4abcf16c3d136a29a2a73fb2552b0d8b
ddargi0410 <ddargi0410@gmail.com>
2026-09-05 19:06:04 +0900

    Add offline service worker

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE = 'school-escape-v1';
+const ASSETS = ['./','./index.html','./styles.css','./game.js','./manifest.webmanifest','./assets/icon.svg','./assets/opening-concept.jpg'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE).then(c => c.put(e.request, copy));
+    return res;
+  }).catch(() => caches.match('./index.html'))));
+});
